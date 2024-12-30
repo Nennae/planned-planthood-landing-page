19904aa42d7b174fd352bfcbb64a07d39337c7fe
Nennae <sezenerdal@outlook.com>
2024-12-30 17:45:48 +0100

    responsive on desktop, tablet, mobile

--- scripts/app.js
+++ scripts/app.js
@@ -11,13 +11,20 @@ function displayProducts(products) {
       products.forEach((product) => {
       
             const productDiv = document.createElement("div");
+            productDiv.classList.add("product-container")
             productDiv.innerHTML = `
-            <img src="${product.image}" alt=${product.name}/>
-            <p class="categories">${product.categories[0].id}</p>
-            <p class="categories">${product.categories[1].id}</p>
-            <p class="categories">${product.categories[2].id}</p>
-            <h2 class="product-name">${product.name}</h2>
-            <p>${product.about}</p>
+            <img class="product-img" src="${product.image}" alt=${product.name}/>
+            <div class="categories">
+            <p class="category">${product.categories[0].id}</p>
+            <p class="category">${product.categories[1].id}</p>
+            <p class="category">${product.categories[2].id}</p>
+            </div>
+            <h3 class="product-name">${product.name}</h3>
+            <p class="about">${product.about}</p>
+            <div class="userinfo">
+            <p class="date">${product.date}</p>
+            <p class="user">${product.user}</p>
+            </div>
             `;
             container.appendChild(productDiv);
       });
