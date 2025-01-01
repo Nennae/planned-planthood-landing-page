580900906322a3d98e0a706e6242c9df99b081de
Nennae <sezenerdal@outlook.com>
2025-01-01 22:31:18 +0100

    function to match color + category

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,34 @@
 import { fetchData } from "./data.js";
 
-async function  init() {
-      const products = await fetchData();
-      displayProducts(products);
+async function init() {
+  const products = await fetchData();
+  displayProducts(products);
 }
 
 function displayProducts(products) {
-      const container = document.getElementById("product-section");
+  const container = document.getElementById("product-section");
 
-      products.forEach((product) => {
-      
-            const productDiv = document.createElement("div");
-            productDiv.classList.add("product-container")
-            productDiv.innerHTML = `
-            <img class="product-img" src="${product.image}" alt=${product.name}/>
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product-container");
+    productDiv.innerHTML = `
+    <img class="overlay-icon" src="/Assets/icons/icon-seedling-2.svg"/>
+            <img class="product-img" src="${product.image}" alt=${
+      product.name
+    }/>
             <div class="categories">
             <p class="category">${product.categories[0].id}</p>
             <p class="category">${product.categories[1].id}</p>
             <p class="category">${product.categories[2].id}</p>
+            ${(() => {
+              if (product.categories[3]) {
+                return `
+                  <p class="category">${product.categories[3].id}</p>
+            `;
+              } else {
+                return ` `;
+              }
+            })()}
             </div>
             <h3 class="product-name">${product.name}</h3>
             <p class="about">${product.about}</p>
@@ -26,8 +37,58 @@ function displayProducts(products) {
             <p class="user">${product.user}</p>
             </div>
             `;
-            container.appendChild(productDiv);
+
+    container.appendChild(productDiv);
+
+    function assignColors() {
+      const categoryElements = productDiv.getElementsByClassName("category");
+
+      Array.from(categoryElements).forEach((element) => {
+        switch (element.innerHTML) {
+          case "Tålig":
+            element.style.backgroundColor = "hsl(221, 63%, 32%)";
+            break;
+          case "Grönska":
+            element.style.backgroundColor = "hsl(110, 63%, 32%)";
+            break;
+          case "Blommor":
+            element.style.backgroundColor = "hsl(262, 89.70%, 49.60%)";
+            break;
+          case "Färgstark":
+            element.style.backgroundColor = "hsl(303, 55%, 47%)";
+            break;
+          case "Solälskande":
+            element.style.backgroundColor = "hsl(26, 93.40%, 41.60%)";
+            break;
+          case "Dekorativ":
+            element.style.backgroundColor = "hsl(278, 79%, 40%)";
+            break;
+          case "Ätbart":
+            element.style.backgroundColor = "hsl(180, 92.40%, 31.00%)";
+            break;
+          case "Lättodlat":
+            element.style.backgroundColor = "hsl(224, 86%, 58%)";
+            break;
+          case "Doftande":
+            element.style.backgroundColor = "hsl(327, 51%, 47%)";
+            break;
+          case "Ormbunke":
+            element.style.backgroundColor = "hsl(180, 84%, 27%)";
+            break;
+          case "Kryddor":
+            element.style.backgroundColor = "hsl(199, 87.30%, 30.80%)";
+            break;
+          case "Sallad":
+            element.style.backgroundColor = "hsl(224, 86%, 58%)";
+            break;
+          default:
+            element.style.backgroundColor = "gray";
+            break;
+        }
       });
+    }
+    assignColors();
+  });
 }
 
 init();
